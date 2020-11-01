209428aa331867ec03aa299a810c39c94fb1fb39
mark22312 <mark22312@gmail.com>
2020-11-01 22:51:25 +0200

    Added base setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-	  radiosavtaPlayer: './src/index.ts'
+	  radiosavtaPlayer: './src/index.tsx'
   },
   module: {
     rules: [
@@ -13,7 +13,18 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'awesome-typescript-loader',
         exclude: /node_modules/,
-      },
+	  },
+	  {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
     ],
   },
   resolve: {
@@ -31,13 +42,17 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
 	  title: "Radiosavta Player tester",
 	  minify: false,
+	  templateContent: `<html><body><div id="Radio-savta"></div></body></html>`
 	  
-  })],
+  }),
+	 new Dotenv()
+],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
 	port: 3000,
 	open: true,
 	injectClient: false
-  }
+  },
+  devtool: 'eval-source-map'
 };
